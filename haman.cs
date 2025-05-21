2b3e87e5d6cf23e2f4f2903ccbb06e142fb1f16f
Arie <s7925al@gmail.com>
2025-05-21 10:13:55 +0300

    class haman first

---
+++ haman.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation
+{
+    public class Haman
+    {
+        public string NameTroorist;
+        public string LastLocatuon;
+        public string TimeInformation;
+        public Haman(string nameTrrorist, string lastLocatuon, string tiimeInformation)
+        {
+            NameTroorist = nameTrrorist;
+            LastLocatuon = lastLocatuon;
+            TimeInformation = tiimeInformation;
+        }
+
+        public Masseg
+    }
+}
