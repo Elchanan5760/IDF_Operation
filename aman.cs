0bac66674d3781166aa944405fddd29d0d0c4df7
Elchanan5760 <emstone200@gmail.com>
2025-05-21 17:06:02 +0300

    Update and rename haman.cs to aman.cs

--- aman.cs
+++ aman.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IDF_Operation
 {
-    public class Haman
+    public class Aman
     {
         public string NameTroorist;
         public string LastLocatuon;
