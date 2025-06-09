aec23da97f17f70b1179d580f18c0a1a03ea6420
Arie <s7925al@gmail.com>
2025-06-09 15:56:38 +0300

    conn and close conn

---
+++ source/repos/Malshinon/mysql/mysql.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+namespace Malshinon.mysql
+{
+    public class mysql
+    {
+        static string connectionString = "Server=localhost;Database=Malshinon;User=rootPasswprd='';Port=3306;";
+        static MySqlConnection connection;
+        static MySqlConnection GetConnnection()
+        {
+            try
+            {
+                var conn = new MySqlConnection()
+            }
+        }
